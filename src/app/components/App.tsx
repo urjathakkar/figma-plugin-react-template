c7a504ce146708232c9be52ee2d074bdfe54325b
Urja Thakkar <thakkarurjaa@gmail.com>
2023-10-20 13:54:41 -0400

    figma rename instances plugin

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,45 +1,89 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, {useState} from 'react';
 import '../styles/ui.css';
+import 'figma-plugin-ds/dist/figma-plugin-ds.css'
+
 
 function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+  const [isComponentSelected, setSelectedComponent] = useState(null);
+  let resetcomponentNameValue = null;
+  let selectedcomponentNameValue = '';
+  const resetcomponentNameRef = React.useRef<HTMLInputElement>(undefined);
+  const selectedcomponentNameRef = React.useRef<HTMLParagraphElement>(undefined);
 
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
-  }, []);
+  const resetInstance = () => {
+    resetcomponentNameValue = resetcomponentNameRef.current.value;
+    parent.postMessage({ pluginMessage: { type: 'resetInstance', resetComponentNameValue: resetcomponentNameValue} }, '*');
+  }  
 
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
-  };
-
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
-  };
+  const resetInstances = () => {
+    resetcomponentNameValue = resetcomponentNameRef.current.value;
+    parent.postMessage({ pluginMessage: { type: 'resetInstances', resetComponentNameValue: resetcomponentNameValue} }, '*');
+    // resetcomponentNameRef.current.value = resetcomponentNameValue;
+  }  
 
   React.useEffect(() => {
     // This is how we read messages sent from the plugin controller
+
     window.onmessage = (event) => {
       const { type, message } = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
-        console.log(`Figma Says: ${message}`);
+      
+      if (type === 'resetInstances' || type === 'resetInstance') {
+        console.log(`Figma Says: Instance/s Renamed`);
+      }
+      if (type === 'instanceSelected') {
+        if(message){
+          // when component selected reflect the changes on the UI
+          resetcomponentNameValue = message.mainComponentName;
+          resetcomponentNameRef.current.value = resetcomponentNameValue;
+          selectedcomponentNameValue = message.selectedComponentName;
+          selectedcomponentNameRef.current.innerHTML = selectedcomponentNameValue;
+          setSelectedComponent(true);
+        }
+        else {
+          // No component selected - reset values
+          resetcomponentNameValue = null;
+          resetcomponentNameRef.current.value = '';
+          selectedcomponentNameValue = null
+          selectedcomponentNameRef.current.innerHTML = '';
+          setSelectedComponent(false);
+          console.log(`No Component Selected`);
+        }
       }
     };
   }, []);
 
   return (
     <div>
-      <img src={logo} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
-      <button onClick={onCancel}>Cancel</button>
+       <p className="type--xlarge mrg8-B">🔥 How to use:</p>
+          <ol className="mrg0-T">
+            <li className="type--small gray">Select one or more layers.</li>
+            <li className="type--small gray">Run this plugin.</li>
+            <li className="type--small gray">Enter new component name</li>
+            <li className="type--small gray">Rename one particular instance or rename all instances on that page</li>
+            <li className="type--small gray">Enjoy🤟</li>
+          </ol>
+        
+        
+        <div className="row column-gap-1">
+
+          {/* User Selected Component */}
+          <p className="type--xlarge type--bold	green column">Component Selected:</p>
+          {!isComponentSelected && <p className="type--small red">Please select an instance</p>}
+          <p ref={selectedcomponentNameRef} id="selectedComponentName" className="type--xlarge"></p>
+        </div>
+        
+        {/* Main Component Name */}
+        <div className="row column-gap-1">
+          <p className="type--xlarge type--bold	green column">Reset Name to:</p>
+          {/* <p ref={resetcomponentNameRef} id="resetComponentName" className="type--xlarge"></p> */}
+          <input ref={resetcomponentNameRef} id="resetComponentName" className="input__field type--xlarge" />
+        </div>
+
+        {/* Actions */}
+        <div className="row column-gap-1 space-around mrg24-T">
+          <button className='button button--primary' onClick={resetInstance}>Reset This Instance</button>
+          <button className='button button--secondary' onClick={resetInstances}>Reset All Instances</button>
+        </div>
     </div>
   );
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,25 +1,53 @@
 figma.showUI(__html__);
+figma.ui.resize(410,350)
+let selectedComponent;
+let mainComponent;
+
+function getSelection() {
+  // get the user's selcetion and the main component of that selection
+  if(figma.currentPage.selection && figma.currentPage.selection[0] && figma.currentPage.selection[0].type === 'INSTANCE')
+  {
+    selectedComponent =  figma.currentPage.selection[0];
+    mainComponent = figma.currentPage.selection[0].mainComponent.parent ? figma.currentPage.selection[0].mainComponent.parent: figma.currentPage.selection[0].mainComponent;
+    figma.ui.postMessage({ type: 'instanceSelected', message: {selectedComponentName: figma.currentPage.selection[0].name, mainComponentName: figma.currentPage.selection[0].mainComponent.parent ? figma.currentPage.selection[0].mainComponent.parent.name: figma.currentPage.selection[0].mainComponent.name} });
+  }
+  else
+  figma.ui.postMessage({ type: 'instanceSelected', message: null });
+}
+
+
+figma.on("selectionchange", () => { 
+  getSelection();
+})
+
+figma.on("run", () => { 
+  getSelection();
+})
 
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = [];
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+  if (msg.type === 'resetInstance') {
+    // selectedComponent.name = mainComponent.name
+    selectedComponent.name = msg.resetComponentNameValue
+    figma.ui.postMessage({ type: 'resetInstance' });
+    figma.closePlugin("Instance Renamed")
+  }
+
+  if (msg.type === 'resetInstances') {
+    const page = figma.currentPage;
+    // && node.mainComponent.parent && node.mainComponent.parent.name === mainComponent.name
+    const instances = page.findAll((node:any) => node.mainComponent && node.mainComponent.remote);
+    let count = 0
+    instances.forEach((instance:any) =>{
+      if(instance.name != mainComponent.name){
+      count +=1
+      // instance.name = mainComponent.name
+      instance.name = msg.resetComponentNameValue
+      }
     }
-
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-
-    // This is how figma responds back to the ui
-    figma.ui.postMessage({
-      type: 'create-rectangles',
-      message: `Created ${msg.count} Rectangles`,
-    });
+    )
+    figma.ui.postMessage({ type: 'resetInstances' });
+    figma.closePlugin(count + " Instances Renamed")
   }
 
   figma.closePlugin();
